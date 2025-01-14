25f81090e694413c8d639c05a0d5d56f3d5b1368
Alexander <alex.lopez.ve@gmail.com>
2025-01-14 17:19:10 +0000

    [dev]: Implemented ft_strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alexander <alexander@student.42.fr>        +#+  +:+       +#+        */
+/*   By: vboxuser <vboxuser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 00:35:48 by alexander         #+#    #+#             */
-/*   Updated: 2024/12/04 00:38:53 by alexander        ###   ########.fr       */
+/*   Updated: 2025/01/14 17:15:02 by vboxuser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,9 @@ char	*ft_strdup(const char *s)
 	char	*str;
 
 	len = ft_strlen(s);
-	str = malloc(sizeof(char) * len);
+	str = malloc(sizeof(char) * (len + 1));
 	if (str == 0)
 		return (NULL);
-	i = 0;
-	while (i < len)
-	{
-		str[i] = s[i];
-		++i;
-	}
+	ft_memcpy(str, s, len + 1);
 	return (str);
 }
