3515fbaff35a88ebe4b91c9fc25b83b1e4ffda10
Alexander <alex.lopez.ve@gmail.com>
2025-01-15 13:41:44 +0000

    [dev]: Added more tests to ft_atoi

--- tests/test_ft_atoi.c
+++ tests/test_ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: vboxuser <vboxuser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 12:30:31 by vboxuser          #+#    #+#             */
-/*   Updated: 2025/01/15 12:51:58 by vboxuser         ###   ########.fr       */
+/*   Updated: 2025/01/15 13:12:45 by vboxuser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,20 @@ extern	int	ft_atoi(const char *str);
 
 void	perform_test(const char *input, int expected)
 {
-	int	result;
+	int		result;
+	char	*print_result;
 
 	result = ft_atoi(input);
-	printf("Input:\t %s\n\tExpected: %d\n\tResult: %d\n", input, expected, result);
+	if (result == expected)
+		print_result = "\033[0;32m\u2713\033[0m";
+	else
+		print_result = "\033[0;31m\u274C\033[0m";
+	printf("%s\t\"%s\"\n", print_result, input);
 }
 
 void	test_ft_atoi(void)
 {
+	printf("Testing ft_atoi\n");
 	perform_test("123", 123);
 	perform_test("-123", -123);
 	perform_test("0", 0);
@@ -42,7 +48,7 @@ void	test_ft_atoi(void)
 	perform_test("+123", 123);
 	perform_test("2147483647", 2147483647);
 	perform_test("-2147483648", -2147483648);
-	perform_test("2147483648", -2147483647);
+	perform_test("2147483648", -2147483648);
 	perform_test("-2147483649", 2147483647);
 	perform_test("123\0456", 123);
 	perform_test("--123", 0);
