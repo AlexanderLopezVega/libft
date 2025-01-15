450a05e295d920e27b8f6ed044de4c68b95bc2dd
Alexander <alex.lopez.ve@gmail.com>
2025-01-15 13:41:17 +0000

    [dev]: Updated import for ft_strdup.c

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,10 +6,11 @@
 /*   By: vboxuser <vboxuser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 00:35:48 by alexander         #+#    #+#             */
-/*   Updated: 2025/01/14 17:21:53 by vboxuser         ###   ########.fr       */
+/*   Updated: 2025/01/15 12:57:34 by vboxuser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include "libft.h"
 
 char	*ft_strdup(const char *s)
