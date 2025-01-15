e586196632662ad912c775746e6d8f78da5441b6
Alexander <alex.lopez.ve@gmail.com>
2025-01-15 12:33:47 +0000

    [dev]: Added tests for validation

---
+++ tests/test.h
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vboxuser <vboxuser@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/14 16:20:52 by vboxuser          #+#    #+#             */
+/*   Updated: 2025/01/15 12:31:54 by vboxuser         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TEST_H
+# define TEST_H
+# include "../libft.h"
+
+void	test_ft_atoi(void);
+void	test_ft_bzero(void);
+void	test_ft_calloc(void);
+void	test_ft_isalnum(void);
+void	test_ft_isalpha(void);
+void	test_ft_isascii(void);
+void	test_ft_isdigit(void);
+void	test_ft_memchr(void);
+void	test_ft_memcmp(void);
+void	test_ft_memcpy(void);
+void	test_ft_memmove(void);
+void	test_ft_memset(void);
+void	test_ft_strchr(void);
+void	test_ft_strdup(void);
+void	test_ft_strlcat(void);
+void	test_ft_strlcpy(void);
+void	test_ft_strlen(void);
+void	test_ft_strncmp(void);
+void	test_ft_strnstr(void);
+void	test_ft_strrchr(void);
+void	test_ft_tolower(void);
+void	test_ft_toupper(void);
+
+#endif
