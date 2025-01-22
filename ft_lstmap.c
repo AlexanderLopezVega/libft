5f72ceb59589387d5263b7167c3199299d241613
Alexander Lopez Vega <alopez-v@student.42madrid.com>
2025-01-22 10:49:37 +0100

    [dev]: Fixed special case in lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: alopez-v <alopez-v@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 11:20:23 by alopez-v          #+#    #+#             */
-/*   Updated: 2025/01/22 09:54:19 by alopez-v         ###   ########.fr       */
+/*   Updated: 2025/01/22 10:49:22 by alopez-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,27 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*map_lst;
 	t_list	*new_node;
+	void	*map_content;
 
 	if (!lst || !f || !del)
 		return (NULL);
 	map_lst = NULL;
 	while (lst)
 	{
-		new_node = ft_lstnew(f(lst->content));
-		if (!new_node)
+		map_content = f(lst->content);
+		if (map_content)
 		{
-			ft_lstclear(&map_lst, del);
-			return (NULL);
+			new_node = ft_lstnew(map_content);
+			if (new_node)
+			{
+				ft_lstadd_back(&map_lst, new_node);
+				lst = lst->next;
+				continue ;
+			}
+			del(map_content);
 		}
-		ft_lstadd_back(&map_lst, new_node);
-		lst = lst->next;
+		ft_lstclear(&map_lst, del);
+		return (NULL);
 	}
 	return (map_lst);
 }
